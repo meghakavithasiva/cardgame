b04a0239784a999046648a415f37cad030faa38a
Siva Kumar <siva.kumar@sky.uk>
2019-06-27 15:17:46 +0100

    static method to create and write user to file

--- user/user.py
+++ user/user.py
@@ -10,9 +10,29 @@ class User:
     def validate_email(self):
         return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", self.email))
 
+    def validate_password(self):
+        return bool(re.search(r"^[a-zA-Z0-9 ]*$", self.password))
+
+    @staticmethod
+    def create_user():
+        name = input("Enter Username: ")
+        email = input("Enter email:   ")
+        pwd = input("Enter password:  ")
+        new_user = User(name, email, pwd)
+        if not new_user.validate_email():
+            raise Exception("Not a valid email!!!")
+        if not new_user.validate_password():
+            raise Exception("Not a valid password!!!")
+        f = open("user.txt", 'a')
+        f.write('\n' + new_user.__str__())
+        f.close()
+        return new_user
+
     def print(self):
         print('Name: {}, Email: {}, Password: {}'.format(self.name, self.email, self.password))
 
+    def __str__(self) -> str:
+        return self.name + '|' + self.email + '|' + self.password
 
-user = User('siva', 'siva@siva.com', 'sivap')
-user.print()
+
+User.create_user()
