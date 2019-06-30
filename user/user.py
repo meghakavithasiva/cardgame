43d1fe49d05f74451e527f57ef3eea56029679c6
Megha Kavitha Siva <megha.kavithasiva@gmail.com>
2019-06-30 16:09:50 +0000

    added user and password validation

--- user/user.py
+++ user/user.py
@@ -1,12 +1,46 @@
 import re
 import subprocess
 import platform
+import os.path
 
 
 class User:
     def __init__(self, name, password):
         self.name = name
+        self.validate_user_length()
+        self.validate_user_duplicate()
         self.password = password
+        self.validate_passsword_length()
+        self.validate_specialcase()
+
+    def validate_user_length(self):
+        name_length = len(self.name)
+        if name_length < 4 or name_length > 10:
+            raise Exception('Sorry, name length must be >=4 and <=10')
+
+    def validate_passsword_length(self):
+        password_length = len(self.password)
+        if password_length < 6 or password_length > 8:
+            raise Exception('Sorry, password length must be >=6 and <=8')
+
+    def validate_specialcase(self):
+        if '|' in self.password:
+            raise Exception('Sorry you cannot use this symbol in password')
+        if '|' in self.name:
+            raise Exception('Sorry you cannot use this symbol in username')
+
+
+    def validate_user_duplicate(self):
+        if os.path.isfile('user.txt'):
+            with open('user.txt', 'r') as f:
+                for line in f:
+                    splitted = line.split('|')
+                    if self.name == splitted[0]:
+                        raise Exception('Duplicate user name')
+                    else:
+                        print('user name is not duplicate')
+        else:
+            print('no validations')
 
     def validate_password(self):
         return bool(re.search(r"^[a-zA-Z0-9 ]*$", self.password))
@@ -39,5 +73,5 @@ class User:
 
 
 user1 = User.create_user()
-User.cls()
-user1.print()
+
+
